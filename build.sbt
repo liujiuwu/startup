d87b6546e8e584ad860d11edccb7e0010429a26a
liujiuwu <liujiuwu@gmail.com>
2013-01-05 15:12:49 +0800

    statup

--- build.sbt
+++ build.sbt
@@ -6,12 +6,18 @@ organization := "com.startup"
 
 scalaVersion := "2.9.2"
 
+//resolvers ++= Seq(
+//"snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+//"releases" at "http://oss.sonatype.org/content/repositories/releases",
+//"Media4u101 SNAPSHOT Repository" at "http://www.media4u101.se:8081/nexus/content/repositories/snapshots/",
+//"Media4u101 Repository" at "http://www.media4u101.se:8081/nexus/content/repositories/releases/"
+//)
+
 resolvers ++= Seq(
-"snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
-"releases" at "http://oss.sonatype.org/content/repositories/releases",
-"Media4u101 SNAPSHOT Repository" at "http://www.media4u101.se:8081/nexus/content/repositories/snapshots/",
-"Media4u101 Repository" at "http://www.media4u101.se:8081/nexus/content/repositories/releases/"
+"small nexus" at "http://58.61.152.91:10004/nexus/content/groups/public/"
 ) 
+
+
 
 seq(com.github.siasia.WebPlugin.webSettings :_*)
 
@@ -27,13 +33,17 @@ scalacOptions ++= Seq("-deprecation", "-unchecked")
 
 (LessKeys.filter in (Compile, LessKeys.less)) := "bootstrap.less"
 
+EclipseKeys.withSource := true
+
 libraryDependencies ++= {
   val liftVersion = "2.5-M3"
   Seq(
-    "net.liftweb"       %% "lift-webkit"        % liftVersion        % "compile",
-    "net.liftweb"       %% "lift-mapper"        % liftVersion        % "compile",
+    "net.liftweb"       %% "lift-webkit"        % liftVersion         % "compile->default"  withSources(),
+    "net.liftweb"       %% "lift-mapper"        % liftVersion        % "compile->default"  withSources(),
+    //"net.liftweb"       %% "lift-record"        % liftVersion        % "compile",
     "net.liftmodules"   %% "lift-jquery-module" % (liftVersion + "-2.0"),
     "net.liftmodules"   %% "widgets" % (liftVersion + "-1.1"),
+    "mysql" 	          %  "mysql-connector-java"   % "5.1.21",
     "org.eclipse.jetty" % "jetty-webapp"        % "8.1.7.v20120910"  % "container,test",
     "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container,test" artifacts Artifact("javax.servlet", "jar", "jar"),
     "ch.qos.logback"    % "logback-classic"     % "1.0.6",
--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,56 +1,58 @@
 package bootstrap.liftweb
 
+import code.model.MyDBVendor
 import net.liftweb._
-import util._
-import Helpers._
-
-import common._
-import http._
-import js.jquery.JQueryArtifacts
-import sitemap._
-import Loc._
-import mapper._
-
-import code.model._
+import net.liftweb.common._
+import net.liftweb.http._
+import net.liftweb.mapper.DB
+import net.liftweb.mapper.DefaultConnectionIdentifier
+import net.liftweb.mapper.Schemifier
+import net.liftweb.sitemap._
+import net.liftweb.sitemap.Loc._
+import net.liftweb.util._
+import net.liftweb.util.Helpers._
 import net.liftmodules.JQueryModule
+import net.liftweb.http.js.jquery.JQueryArtifacts
+import code.model.User
 
 
 
 class Boot {
   def boot {
-    // where to search snippet
     LiftRules.addToPackages("code")
+    DB.defineConnectionManager(DefaultConnectionIdentifier, MyDBVendor)
 
-    // Build SiteMap
-    def sitemap = SiteMap(
-      Menu.i("Home") / "index"
-    )
-
-    // set the sitemap.  Note if you don't want access control for
-    // each page, just comment this line out.
-    LiftRules.setSiteMapFunc(() => sitemap)
-
-    //Init the jQuery module, see http://liftweb.net/jquery for more information.
     LiftRules.jsArtifacts = JQueryArtifacts
-    JQueryModule.InitParam.JQuery=JQueryModule.JQuery172
+    JQueryModule.InitParam.JQuery=JQueryModule.JQuery182
     JQueryModule.init()
 
-    //Show the spinny image when an Ajax call starts
-    LiftRules.ajaxStart =
-      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-    
-    // Make the spinny image go away when it ends
-    LiftRules.ajaxEnd =
-      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
-
-    // Force the request to be UTF-8
+    LiftRules.ajaxStart = Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+    LiftRules.ajaxEnd = Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
-
-    // What is the function to test if a user is logged in?
-    LiftRules.loggedInTest = Full(() => User.loggedIn_?)
-
-    // Use HTML5 for rendering
-    LiftRules.htmlProperties.default.set((r: Req) =>
-      new Html5Properties(r.userAgent))    
+    LiftRules.htmlProperties.default.set((r: Req) => new Html5Properties(r.userAgent))  
+    
+    Schemifier.schemify(true, Schemifier.infoF _, User)
+    LiftRules.setSiteMapFunc(() => MenuInfo.sitemap)
   }
+}
+
+object MenuInfo {
+  import Loc._
+
+  val adminMenus = Menu("管理首页") / "admin" / "index" >> Hidden submenus (
+    Menu("分类管理") / "admin" / "sections" / "index" >> LocGroup("admin"),
+    Menu("新建分类") / "admin" / "sections" / "add" >> LocGroup("admin"),
+    Menu("修改分类") / "admin" / "sections" / "edit" >> Hidden,
+    Menu("删除分类") / "admin" / "sections" / "delete" >> Hidden,
+
+    Menu("讨论节点管理") / "admin" / "nodes" / "index" >> LocGroup("admin"),
+    Menu("新建讨论节点") / "admin" / "nodes" / "add" >> LocGroup("admin"),
+    Menu("修改讨论节点") / "admin" / "nodes" / "edit" >> Hidden,
+    Menu("删除讨论节点") / "admin" / "nodes" / "delete" >> Hidden)
+
+  def sitemap() = SiteMap(
+    Menu("Home") / "index",
+    Menu("Signup") / "sign_up",
+    adminMenus,
+    Menu(Loc("help", ("help" :: Nil) -> true, "帮助")))
 }
