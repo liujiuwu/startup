de8e573975847427d59f87e8525868272ba794a7
liujiuwu <liujiuwu@gmail.com>
2013-01-09 18:19:57 +0800

    startup

--- src/main/scala/code/snippet/HelloWorld.scala
+++ src/main/scala/code/snippet/HelloWorld.scala
@@ -1,5 +1,5 @@
-package code 
-package snippet 
+package code
+package snippet
 
 import scala.xml.{NodeSeq, Text}
 import net.liftweb.util._
@@ -8,6 +8,21 @@ import java.util.Date
 import code.lib._
 import Helpers._
 import net.liftmodules.widgets.gravatar.Gravatar
+
+import net.liftweb.http.js._
+import JsCmds._
+
+import _root_.net.liftweb.http._
+import S._
+import SHtml._
+import js._
+import js.jquery._
+import JqJsCmds._
+import JsCmds._
+import util._
+import Helpers._
+import _root_.scala.xml.{Text, NodeSeq}
+
 
 class HelloWorld {
   lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
@@ -21,6 +36,19 @@ class HelloWorld {
    def howdy = "#time *" #> date.toString
    */
 
-   def myface = Gravatar("liujiuwu@gmail.com")
-}
+  def myface = Gravatar("liujiuwu@gmail.com")
 
+  var cnt = 0
+  val spanName: String = S.attr("id_name") openOr "cnt_id"
+
+  def doClicker(text: NodeSeq) =
+    a(() => {
+      cnt = cnt + 1;
+      SetHtml(spanName, Text(cnt.toString))
+    }, text)
+
+  def ajaxTest(xhtml: NodeSeq): NodeSeq = {
+    // bind the view to the functionality
+    bind("ajax", xhtml,"clicker" -> doClicker _)
+  }
+}
