bd7a120f23b7ff468ffe97a720df7bb13d44cc1e
liujiuwu <liujiuwu@gmail.com>
2013-01-13 09:24:59 +0800

    clean

---
+++ src/main/scala/code/lib/ProjectStatus.scala
@@ -0,0 +1,12 @@
+package code.lib
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jiuwu
+ * Date: 13-1-12
+ * Time: 下午3:22
+ * To change this template use File | Settings | File Templates.
+ */
+object ProjectStatus {
+
+}
